e44b6367ab133fb0a39bcd835cf59972e4c43266
Kevin LaFlamme <laflamme.kevin@gmail.com>
2014-03-31 11:37:19 -0400

    Added string to UTF-8 byte array conversion and vice versa

--- jsEncoder.js
+++ jsEncoder.js
@@ -8,29 +8,28 @@ var jsEncoder = (function() {
     'use strict';
     
     //
-    //    All of these functions currently use strings and charCodeAt
-    //    and assume a single byte character. Additional steps are needed
-    //    to convert to byte arrays first.
-    //
-
-    //
     //    Base64 encode/decode functions
     //
     
     //    Base64 character array
-    var repStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    var b64Str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     
-    
+    //
     //    Public
     //    Base64 encode
-     
+    //
     var encode64 = function( data ) {
     
-        var c1, c2, c3, e1, e2, e3, e4;
-        var i   = 0;
-        var out = "";
-        var len = data.length;
-        var rem = len % 3;
+        var c1, c2, c3, e1, e2, e3, e4,
+            i   = 0,
+            out = "",
+            len = data.length,
+            rem = len % 3;
+        
+        if( !isArray( data ) ) {
+            
+            throw new Error( "TypeError - Input must be an array" );
+        }
         
         //
         //  Pad with nulls until length is 
@@ -38,28 +37,27 @@ var jsEncoder = (function() {
         //
         if( rem === 1 ) {
             
-            data = data.concat( "\x00\x00" );
+            data.push( 0, 0 );
             len += 2;
             
         } else if( rem === 2 ) {
             
-            data = data.concat( "\x00" );
+            data.push( 0 );
             len += 1;
         }             
         
-        
         while( i < len ) {
             
-            c1 = data.charCodeAt(i++);
-            c2 = data.charCodeAt(i++);
-            c3 = data.charCodeAt(i++);
+            c1 = data[ i++ ];
+            c2 = data[ i++ ];
+            c3 = data[ i++ ];
             
             e1 = c1 >> 2;
             e2 = ((c1 << 4) & 48) | (c2 >> 4);
             e3 = ((c2 << 2) & 60) | (c3 >> 6);
             e4 = c3 & 63;
             
-            out += (repStr.charAt(e1) + repStr.charAt(e2) + repStr.charAt(e3) + repStr.charAt(e4));
+            out += ( b64Str.charAt( e1 ) + b64Str.charAt( e2 ) + b64Str.charAt( e3 ) + b64Str.charAt( e4 ) );
         }
         
         //
@@ -68,11 +66,11 @@ var jsEncoder = (function() {
         //
         if( rem === 1 ) {
             
-            out = out.substring(0, out.length - 2).concat("==");
+            out = out.substring( 0, out.length - 2 ).concat( "==" );
             
         } else if( rem === 2 ) {
             
-            out = out.substring(0, out.length - 1).concat("=");
+            out = out.substring( 0, out.length - 1 ).concat( "=" );
         }
         
         return out;    
@@ -85,33 +83,39 @@ var jsEncoder = (function() {
     // 
     var decode64 = function( data ) {
     
-        var c1, c2, c3, e1, e2, e3, e4;
-        var i   = 0;
-        var out = "";
-        var len = data.length;
+        var e1, e2, e3, e4,
+            i   = 0,
+            len,
+            out = [];
         
         //
         //  Make sure input length is a multiple of 4 and only contains
         //  valid characters
         //
-        if( (len % 4 !== 0) || (/[A-Za-z0-9+=\/]+/).test( data ) )  {
+        if( typeof data !== 'string' || ( len % 4 !== 0 ) || (/[A-Za-z0-9+=\/]+/).test( data ) )  {
             
-            throw new Error("Input not properly encoded");
-            
+            throw new Error( "Input not properly encoded" );            
         }
+        
+        len = data.length;
         
         while( i < len ) {
         
-            e1 = repStr.indexOf(data.charAt(i++));
-            e2 = repStr.indexOf(data.charAt(i++));
-            e3 = repStr.indexOf(data.charAt(i++));
-            e4 = repStr.indexOf(data.charAt(i++));
+            e1 = b64Str.indexOf( data.charAt( i++ ) );
+            e2 = b64Str.indexOf( data.charAt( i++ ) );
+            e3 = b64Str.indexOf( data.charAt( i++ ) );
+            e4 = b64Str.indexOf( data.charAt( i++ ) );
             
-            c1 = (e1 << 2) | (e2 >> 4);
-            c2 = ((e2 << 4) & 240) | (e3 >> 2);
-            c3 = ((e3 << 6) & 192) |  e4;
+            out.push( ( e1 << 2 ) | ( e2 >> 4 ) );
+            if( e3 !== -1 ) {
+                
+                out.push( ((e2 << 4) & 0xf0) | (e3 >> 2) );
+            }
             
-            out += (String.fromCharCode(c1) + (e3 !== -1 ? String.fromCharCode(c2) : "") + (e4 !== -1 ? String.fromCharCode(c3) : ""));
+            if( e4 !== -1 ) {
+                
+                out.push( ((e3 << 6) & 0xc0 ) |  e4 );
+            }
         }
         
         return out;
@@ -122,118 +126,194 @@ var jsEncoder = (function() {
     //    Converts a string to an array of bytes
     //    representing it's utf8 equivalent
     //
+    //    In:: String
+    //   Out:: UTF-8 encoded array of bytes
+    //
     var strToUtf8Array = function( data ) {
         
-        var i, c, len, out = [];
+        var i, c,
+            len,
+            out = [];
         
-        if( typeof data === "string" ) {
+        if( typeof data !== "string" ) {
             
-            len = data.length;
+            throw new Error( "Input must be a string" );            
+        }
             
-            for( i = 0; i < len; i++ ) {
+        len = data.length;
+            
+        for( i = 0; i < len; i++ ) {
                 
-                c = data.charCodeAt(i);
+            c = data.charCodeAt(i);
                 
-                if( c < 0x80 ) {
-                    
-                    // UTF-8 encodes all codepoints < 128 as
-                    // their ASCII equivalent
-                    out.push( c );
-                    
-                } else if( c < 0x800 ) {
-                    
-                    //
-                    // Code point U+07FF uses 11 bits,
-                    // we need the first 5 bits for byte 1,
-                    // so shift right by 6 and add to the leading 
-                    // byte 110xxxxx.
-                    //
-                    // Then  take the last 6 bits and 
-                    // add it to the leading bits for
-                    // the continuation byte 10xxxxxx
-                    //
-                    out.push( 0xc0 | (c >> 6),
-                              0x80 | (c & 0x3f) );
-                    
-                } else if ( c < 0xd800 || c > 0xdfff ) {
-                    
-                    //
-                    // charCodeAt always returns a value <= 0xFFFF
-                    // but code points U+D800 through U+DFFF are 
-                    // reserved for surrogate pairs. This grabs 
-                    // those points from U+0800 through U+D7FF and
-                    // U+E000 through U+FFFF
-                    //
-                    // Code point U+FFFF uses 16 bits, we need
-                    // the first 4 for the leading byte 0x1110xxxx
-                    // the next 6 for the first continuation 
-                    // byte 0x10xxxxxx, and teh last 6 for the 
-                    // second continuation byte 0x10xxxxxx
-                    //
-                    out.push( 0xe0 | (c >> 12),
-                              0x80 | ((c >> 6) & 0x3f),
-                              0x80 | (c & 0x3f) );
-                } else {
-                    
-                    // Surrogate pair
-                    // The code point is broken into two pieces, by first 
-                    // subtracting 0x10000 leaving you with a 20 bit number.
-                    //
-                    // The first 10 bits are are added to the 0xD800 and the 
-                    // last 10 bits are added to 0xDC00. This results in two
-                    // separate 2 byte values in the reserved range 
-                    // U+D800 through U+DFFF.
-                    //
-                    // Reverse the process to get the code point, up 
-                    // to U+1FFFFF which uses 21 bits, so we encode 
-                    // by combining first 3 bits with leading byte 0x11110xxx,
-                    // next 6 bits with continuation byte 0x10xxxxxx,
-                    // next 6 bits with continuation byte 0x10xxxxxx,
-                    // and last 6 bits with continuation byte 0x10xxxxxx
-                    c = ((c - 0xd800) << 10)  & (data.charCodeAt(i++) - 0xdc00) + 0x10000;
-                    
-                    out.push( 0x1e | (c >> 18),
-                              0x80 | ((c >> 12) & 0x3f),
-                              0x80 | ((c >> 6) & 0x3f),
-                              0x80 | (c & 0x3f) );                    
-                }
+            if( c < 0x80 ) {
                 
-            }
-            
-            return out;            
-            
-        } else {
-            
-            throw new Error("Input must be a string");
-            
+                // UTF-8 encodes all codepoints < 128 as
+                // their ASCII equivalent
+                out.push( c );
+                    
+            } else if( c < 0x800 ) {
+                    
+                //
+                // Code point U+07FF uses 11 bits,
+                // we need the first 5 bits for byte 1,
+                // so shift right by 6 and add to the leading 
+                // byte 110xxxxx.
+                //
+                // Then  take the last 6 bits and 
+                // add it to the leading bits for
+                // the continuation byte 10xxxxxx
+                //
+                out.push( 0xc0 | (c >> 6),
+                          0x80 | (c & 0x3f) );
+                    
+             } else if ( c < 0xd800 || c > 0xdfff ) {
+                    
+                 // charCodeAt always returns a value <= 0xFFFF
+                 // but code points U+D800 through U+DFFF are 
+                 // reserved for surrogate pairs. This grabs 
+                 // those points from U+0800 through U+D7FF and
+                 // U+E000 through U+FFFF
+                 //
+                 // Code point U+FFFF uses 16 bits, we need
+                 // the first 4 for the leading byte 0x1110xxxx
+                 // the next 6 for the first continuation 
+                 // byte 0x10xxxxxx, and the last 6 for the 
+                 // second continuation byte 0x10xxxxxx
+                 //
+                 out.push( 0xe0 | (c >> 12),
+                           0x80 | ((c >> 6) & 0x3f),
+                           0x80 | (c & 0x3f) );
+              } else {
+                    
+                  // Surrogate pair
+                  // The code point is broken into two pieces, by first 
+                  // subtracting 0x10000 leaving you with a 20 bit number.
+                  //
+                  // The first 10 bits are are added to the 0xD800 and the 
+                  // last 10 bits are added to 0xDC00. This results in two
+                  // separate 2 byte values in the reserved range 
+                  // U+D800 through U+DFFF.
+                  //
+                  // Reverse the process to get the code point, up 
+                  // to U+1FFFFF which uses 21 bits, so we encode 
+                  // by combining first 3 bits with leading byte 0x11110xxx,
+                  // next 6 bits with continuation byte 0x10xxxxxx,
+                  // next 6 bits with continuation byte 0x10xxxxxx,
+                  // and last 6 bits with continuation byte 0x10xxxxxx
+                  //
+                  c = (((c - 0xd800) << 10) | (data.charCodeAt(++i) - 0xdc00)) + 0x10000;
+                  
+                  out.push( 0xf0 | (c >> 18),
+                            0x80 | ((c >> 12) & 0x3f),
+                            0x80 | ((c >> 6) & 0x3f),
+                            0x80 | (c & 0x3f) );                    
+              }
         }
         
+        return out; 
+    };
+    
+    //
+    //    Public
+    //    Convert an array of bytes that represent utf-8 encoding
+    //    to a string
+    //
+    //     In:: UTF-8 encoded array of bytes
+    //    Out:: String
+    //
+    var utf8ArrayToStr = function( data ) {
+        
+        var i, c, u,
+            len,
+            out = "";
+        
+          if( !isArray( data ) ) {
+            
+            throw new Error( "TypeError - Input must be an array" );
+        }
+          
+          len = data.length;
+          
+          for( i=0; i < len; i++ ) {
+              
+              c = data[ i ];
+              
+              if( c >= 0xf0 ) {
+                  
+                  u  = (c & 0x07) << 18;
+                  u |= ((data[ ++i ] & 0x3f) << 12);
+                  u |= ((data[ ++i ] & 0x3f) << 6);
+                  u |= (data[ ++i ] & 0x3f);
+                  
+              } else if( c > 0xe0 ) {
+                  
+                  u  = (c & 0x0f) << 12;
+                  u |= ((data[ ++i ] & 0x3f) << 6);
+                  u |= (data[ ++i ] & 0x3f);
+                  
+              } else if( c > 0xc0 ) {
+                  
+                  u  = (c & 0x1f) << 6;
+                  u |= (data[ ++i ] & 0x3f);
+              } else {
+                  
+                  u = c;
+              }
+              
+              // If our code point is < U+10000 then we
+              // can just concatenate \uXXXX.
+              //
+              // Otherwise, we have to calculate the 
+              // surrogate pair which is done by 
+              // subtracting 0x10000, leaving a 20 bit
+              // number. The leading surrogate is the first
+              // 10 bits added to U+D800, and the trailing
+              // surrogate is the last 10 bits added to U+DC00
+              //
+              if( u < 0x10000 ) {
+                  
+                  out += String.fromCharCode( u );
+                  
+              } else {
+                  
+                  u -= 0x10000;
+                  out += String.fromCharCode( 0xd800 + (u >>> 10), 0xdc00 + (u & 0x3ff) );
+              }
+          }
+          
+          return out;
     };
     
     //
     //    Public
     //    Convert a 32 bit number to a byte array
     //
+    //    In:: Integer
+    //   Out:: Byte array representation
+    //
     var intToByteArray = function( int ) {
         
         var out = [];
         
+        if( typeof int !== 'number' ) {
+            
+            throw new Error( "TypeError - must be a number" );
+        }
+        
         if( int > 0xFFFFFF ) {
             
-            out.push( int >>> 24 );
-            
+            out.push( int >>> 24 );            
         }
         
         if( int > 0xFFFF ) {
             
-            out.push( (int >>> 16) & 0xFF );
-            
+            out.push( (int >>> 16) & 0xFF );            
         }
         
         if( int > 0xFF ) {
             
-            out.push( (int >> 8) & 0xFF );
-            
+            out.push( (int >> 8) & 0xFF );            
         }
         
         out.push( int & 0xFF );
@@ -250,53 +330,52 @@ var jsEncoder = (function() {
     //
     var crc = function( options ) {
         
-        if( options.width && options.poly ) {
+        var c, i, j, len, mask, crc;
         
-            var len = options.data ? options.data.length : 0;
-        
-            var c, i, j;
-        
-            // To mask off any bits to the left of *width*
-            var mask = (0xFFFFFFFF >>> (32 - options.width));     
-        
-            // If there's an initial value, mask and set initial CRC
-            var crc = (typeof options.init) !== 'undefined' ? (options.init & mask) : (0xFFFFFFFF & mask);
-        
-        
-            for(i = 0; i < len; i++) {
-        
-                c = options.data.charCodeAt(i);
+        if( !(options.width && options.poly) ) {
             
-                if( options.revData ) {
-                
-                    c = revbits(c, 8);
-                
-                }
-                
-                c <<= (options.width - 8);
+            throw new Error( "Error - Invalid options: must provide width and poly" );
             
-                for(j = 0; j < 8; j++) {
+        } else if( !isArray( options.data ) ) {
             
-                    crc = ((crc ^ c) & (1 << (options.width -1))) ? (crc << 1) ^ options.poly : crc << 1;
-                    crc &= mask;
-                    c <<= 1;
-                }
+            throw new Error( "TypeError - data must be an array" );
+        }        
+            
+        len = options.data ? options.data.length : 0;
+                     
+        // To mask off any bits to the left of *width*
+        mask = (0xFFFFFFFF >>> (32 - options.width));     
+        
+        // If there's an initial value, mask and set initial CRC
+        crc = typeof options.init !== 'undefined' ? (options.init & mask) : (0xFFFFFFFF & mask);
+        
+        
+        for( i = 0; i < len; i++ ) {
+        
+            c = options.data[ i ];
+            
+            if( options.revData ) {
+                
+                c = revbits( c, 8 );                
             }
-        
-            if( options.revCrc ) {
+                
+            c <<= (options.width - 8);
             
-                crc = revbits(crc, options.width);
+            for( j = 0; j < 8; j++ ) {
             
+                crc = ((crc ^ c) & (1 << (options.width -1))) ? (crc << 1) ^ options.poly : crc << 1;
+                crc &= mask;
+                c <<= 1;
             }
-            
-        
-            return (crc ^ (options.xor || 0)) >>> 0;
-            
-        } else {
-            
-            throw new Error("Invalid options");
-            
         }
+        
+        if( options.revCrc ) {
+            
+            crc = revbits( crc, options.width );            
+        }
+            
+        
+        return (crc ^ (options.xor || 0)) >>> 0;
     };
     
     //
@@ -368,7 +447,7 @@ var jsEncoder = (function() {
         for(i = 0; i < width; i++)
         {
             reverse <<= 1;
-            if(data & 0x1) reverse++;
+            if( data & 0x1 ) reverse++;
             data >>= 1;
         }
     
@@ -385,6 +464,14 @@ var jsEncoder = (function() {
         return ((revbits( poly, width ) << 1 & (0xFFFFFFFF >>> (32 - width)) ) + 1);
     }
     
+    //
+    //    Private function to determine array
+    //    Thanks to http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
+    //
+    function isArray( object ) {
+        
+        return Object.prototype.toString.call( object ) === '[object Array]';        
+    }
     
     //
     //    Return an object to expose the public
@@ -393,6 +480,9 @@ var jsEncoder = (function() {
     return {
         encode64: encode64,
         decode64: decode64,
+  strToUtf8Array: strToUtf8Array,
+  utf8ArrayToStr: utf8ArrayToStr,
+  intToByteArray: intToByteArray,
        crc_ccitt: crc_ccitt,
            crc32: crc32,
            crc16: crc16,
